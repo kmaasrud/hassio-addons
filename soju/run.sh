cf4b40e4df562b40a4d27e14d0b9cf3fd63e7c07
Knut Magnus Aasrud <km@aasrud.com>
2025-07-31 09:49:00 +0200

    Update soju addon

--- soju/run.sh
+++ soju/run.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/with-contenv bashio
 
-mkdir -p /etc/soju
-echo "listen irc+insecure://localhost" >>/etc/soju/config
-echo "accept-proxy-ip localhost" >>/etc/soju/config
+ADMIN_USER=$(bashio::config 'admin_user')
+ADMIN_PASS=$(bashio::config 'admin_password')
+IRC_PORT=$(bashio::config 'irc_port')
+IRCS_PORT=$(bashio::config 'ircs_port')
 
-/usr/bin/soju
+CONFIG_FILE=/data/soju.conf
+cat >"$CONFIG_FILE" <<EOF
+listen unix+admin://
+listen irc+insecure://0.0.0.0:$IRC_PORT
+${IRCS_PORT:+listen ircs://0.0.0.0:$IRCS_PORT}
+db sqlite3 /data/soju.db
+EOF
+
+sojuctl -config /data/soju.conf user create -admin \
+	-username "$ADMIN_USER" -password "$ADMIN_PASS"
+
+exec soju -config /data/soju.conf
